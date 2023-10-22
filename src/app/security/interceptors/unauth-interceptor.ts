0b275b8294a21f43b75788548f55da398092fecf
Odra Tezo <fatimatezo201831039@cunoc.edu.gt>
2023-10-22 17:11:06 -0600

    🚧 ciudad crud

---
+++ src/app/security/interceptors/unauth-interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CurrentUserService } from 'src/app/services/current-user.service';
+
+@Injectable()
+export class UnauthInterceptor implements HttpInterceptor {
+
+  constructor(
+    private currentUserService: CurrentUserService
+  ) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      tap(
+        _ => { },
+        (err: any) => {
+          if (err instanceof HttpErrorResponse && err.status === 401) {
+            this.currentUserService.logout();
+          }
+        }
+      )
+    );
+  }
+
+}
