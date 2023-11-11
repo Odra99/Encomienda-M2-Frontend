a6ec939d825680125d820960975b035061088424
Odra Tezo <fatimatezo201831039@cunoc.edu.gt>
2023-11-10 20:23:37 -0600

    ⚡️ Adding tarifario list

--- src/app/features/tarifario/tarifario.component.ts
+++ src/app/features/tarifario/tarifario.component.ts
@@ -1,13 +1,14 @@
 import { SelectionModel } from '@angular/cdk/collections';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, NgForm } from '@angular/forms';
 import { MatDatepicker } from '@angular/material/datepicker';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import * as moment from 'moment';
-import { Sucursal } from 'src/app/data/model/general';
+import { Sucursal, Tarifario } from 'src/app/data/model/general';
 import { needConfirmation } from 'src/app/decorators/confirm-dialog.decorator';
 import { SucursalService } from 'src/app/services/backend/sucursal.service';
+import { TarifarioService } from 'src/app/services/backend/tarifario.service';
 import { DialogService } from 'src/app/services/others/dialog.service';
 import { ToasterService } from 'src/app/services/others/toaster.service';
 import { ToasterEnum } from 'src/global/toaster-enum';
@@ -15,44 +16,44 @@ import { ToasterEnum } from 'src/global/toaster-enum';
 @Component({
   selector: 'app-tarifario',
   templateUrl: './tarifario.component.html',
-  styleUrls: ['./tarifario.component.scss']
+  styleUrls: ['./tarifario.component.scss'],
 })
 export class TarifarioComponent implements OnInit, AfterViewInit {
   tabs = 0;
 
   displayedColumns: string[] = [
-    'select',
     'index',
-    'name',
-    'direccion',
-    'ciudad',
-    'tipoSucursal',
+    'fecha',
+    'ganancia_envio',
+    'costo_lb',
     'actions',
   ];
-  
-  datos: Sucursal[] = [];
+
+  datos: Tarifario[] = [];
   @ViewChild('paginator') paginator: MatPaginator;
-  
-  dataSource = new MatTableDataSource<Sucursal>(this.datos);
-  
+
+  dataSource = new MatTableDataSource<Tarifario>(this.datos);
+
+
+  @ViewChild('tarifarioForm', { read: NgForm }) form!: NgForm;
+
   list = true;
-  selectedId:number
-  
-  
+  selectedId: number;
+
+  tarifario: Tarifario = new Tarifario();
 
   constructor(
     private sucursalService: SucursalService,
     private toasterService: ToasterService,
-    private confirmationDialogService: DialogService
-    ) {}
+    private confirmationDialogService: DialogService,
+    private tarifarioService: TarifarioService,
+  ) {}
 
-
- 
   changeTab(num: number) {
     this.tabs = num;
     this.list = true;
   }
-  
+
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
   }
@@ -62,10 +63,10 @@ export class TarifarioComponent implements OnInit, AfterViewInit {
   }
 
   getAll() {
-    this.sucursalService.listAllHttp({}).subscribe({
+    this.tarifarioService.listAllHttp({}).subscribe({
       next: (value) => {
         this.datos = value.body.result;
-        this.dataSource = new MatTableDataSource<Sucursal>(this.datos);
+        this.dataSource = new MatTableDataSource<Tarifario>(this.datos);
         this.dataSource.paginator = this.paginator;
       },
       error: () => {
@@ -74,18 +75,18 @@ export class TarifarioComponent implements OnInit, AfterViewInit {
     });
   }
 
-  edit(id:number){
+  edit(id: number) {
     this.selectedId = id;
     this.list = false;
   }
 
-  setListView(){
+  setListView() {
     this.getAll();
     this.tabs = 0;
     this.list = true;
   }
 
-  selection = new SelectionModel<Sucursal>(true, []);
+  selection = new SelectionModel<Tarifario>(true, []);
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
@@ -93,7 +94,6 @@ export class TarifarioComponent implements OnInit, AfterViewInit {
     const numRows = this.dataSource.data.length;
     return numSelected === numRows;
   }
-
 
   toggleAllRows() {
     if (this.isAllSelected()) {
@@ -114,10 +114,24 @@ export class TarifarioComponent implements OnInit, AfterViewInit {
     if (ctrlValue) {
       ctrlValue.month(normalizedMonthAndYear.month());
       ctrlValue.year(normalizedMonthAndYear.year());
+      this.tarifario.fecha = normalizedMonthAndYear.format();
       this.date.setValue(ctrlValue);
       datepicker.close();
     }
   }
 
-
+  createTarifario() {
+    this.form.form.markAllAsTouched();
+    if (!this.form.form.valid) {
+      return;
+    }
+    this.tarifarioService.save(this.tarifario).subscribe({
+      next: () => {
+        this.toasterService.show({message:"Tarifario calculado con exito",type:ToasterEnum.SUCCESS})
+      },
+      error: () => {
+        this.toasterService.showGenericErrorToast();
+      },
+    });
+  }
 }
