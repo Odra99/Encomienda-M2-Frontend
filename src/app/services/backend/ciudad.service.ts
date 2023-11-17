e80c50dcb19422445ba910c88345c1d9d2a2aaf0
Odra Tezo <fatimatezo201831039@cunoc.edu.gt>
2023-11-17 00:15:55 -0600

    ⚡️ Agregando ciudad y vehiculo al modulo predictivo

--- src/app/services/backend/ciudad.service.ts
+++ src/app/services/backend/ciudad.service.ts
@@ -3,34 +3,45 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Ciudad } from 'src/app/data/model/general';
 import { environment } from 'src/environment/environment';
-
-const baseUrl = environment.encomiendaBackendUrl + 'ciudad';
+import { PredectiveModuleService } from '../others/predictive-module.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CiudadService {
-  constructor(private http: HttpClient) {}
+  baseUrl = environment.encomiendaBackendUrl + 'ciudad';
+  constructor(
+    private http: HttpClient,
+    private predectibleModule: PredectiveModuleService
+  ) {}
+
+  private getUrl() {
+    this.baseUrl = this.predectibleModule.getUrl() + 'ciudad';
+  }
 
 
   save(entity: Ciudad): Observable<any> {
+    this.getUrl();
     if (entity.id) {
-      return this.http.patch<any>(`${baseUrl}/${entity.id}`, entity);
+      return this.http.patch<any>(`${this.baseUrl}/${entity.id}`, entity);
     }
-    return this.http.post<any>(`${baseUrl}`, entity);
+    return this.http.post<any>(`${this.baseUrl}`, entity);
   }
 
   get(id: number): Observable<any> {
-    return this.http.get<any>(`${baseUrl}/${id}`);
+    this.getUrl();
+    return this.http.get<any>(`${this.baseUrl}/${id}`);
   }
 
   delete(id: number): Observable<HttpResponse<any>> {
-    return this.http.delete<HttpResponse<any>>(`${baseUrl}/${id}`);
+    this.getUrl();
+    return this.http.delete<HttpResponse<any>>(`${this.baseUrl}/${id}`);
   }
 
   listAllHttp(queryParams: any): Observable<HttpResponse<any>> {
+    this.getUrl();
     const params = this.getQueryParams(queryParams);
-    return this.http.get<HttpResponse<any>>(baseUrl, {
+    return this.http.get<HttpResponse<any>>(this.baseUrl, {
       params: params,
       observe: 'response',
     });
@@ -38,18 +49,6 @@ export class CiudadService {
 
   private getQueryParams(queryParams: any): HttpParams {
     let params = new HttpParams();
-    if (queryParams['page'] !== undefined)
-      params = params.append('page', queryParams['page']);
-    if (queryParams['max'] !== undefined)
-      params = params.append('max', queryParams['max']);
-    if (queryParams['asc'] !== undefined)
-      params = params.append('asc', queryParams['asc']);
-    if (queryParams['columnOrder'] !== undefined)
-      params = params.append('columnOrder', queryParams['columnOrder']);
-    if (queryParams['myConnections'] !== undefined)
-      params = params.append('myConnections', queryParams['myConnections']);
-    if (queryParams['status'] !== undefined)
-      params = params.append('status', queryParams['status']);
     return params;
   }
 }
